7728e61632c0e073680a8c16e0dcc5ad63cba7dd
Nekbakht Sholangarova <nekbakhtsholangarova@Nekbakhts-MacBook-Air.local>
2023-05-26 19:55:07 -0500

    variable practice

---
+++ 01_variable_dataType/variable.js
@@ -0,0 +1,32 @@
+
+let name = 'Nekbakht';
+console.log(typeof name);
+
+let age = 25;
+console.log(typeof age);
+
+let isStudent = true;
+
+let favoritColors1 = [];
+ 
+let favoritColors = ['red', 'green', 'black', 'blue'];
+console.log(favoritColors);
+console.log('favoritColors');
+console.log(typeof favoritColors);
+
+console.log('\n\nfavoritColors->${favoritColors}');
+
+console.log('typeof favoritColors -> ${typeof favoritColors}\n\n');
+
+let person = {
+    name: 'Nekbakht',
+    age: 25,
+    isStudent: 'true'
+};
+console.log(person);
+console.log('person');
+console.log(typeof person);
+console.log(person.age);
+console.log('My Name is' +  name);
+
+
