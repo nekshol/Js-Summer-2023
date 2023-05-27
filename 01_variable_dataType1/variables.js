198b201e202b8e56753510cb5123afc75a965d2e
Nekbakht <nekbakht@gmail.com>
2023-05-26 21:16:32 -0500

    valiables

--- 01_variable_dataType1/variables.js
+++ 01_variable_dataType1/variables.js
@@ -1,4 +1,3 @@
-
 let name = 'Nekbakht';
 console.log(typeof name);
 
@@ -9,22 +8,13 @@ let isStudent = true;
 console.log(typeof isStudent);
 
 let favoritColors = [];
- 
 let favoritColors2 = ['Red', 'Green', 'Black', 'Blue'];
 
 console.log(typeof favoritColors);
-console.log('\nfavoritColors2 -> ${favoritColors2}');
-
-console.log('typeof favoritColors2 -> ${typeof_favoritColors2}\n');
-
+ 
 let person = {
     name: 'Nekbakht',
     age: 25,
     isStudent: 'true'
-};
-console.log(person);
-console.log('person');
+}
 console.log(typeof person);
-console.log(person.age);
-console.log('My Name is' +  name);
-
