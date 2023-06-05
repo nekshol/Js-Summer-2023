2ade298b1b0c1f9c3653998d0459d0950c8eaf26
Nekbakht <nekbakht@gmail.com>
2023-06-05 17:24:10 -0500

    operators

---
+++ 01_variable_dataType1/operators.js
@@ -0,0 +1,71 @@
+// Arithmetic Operators:
+let num1 = 30;
+let num2 = 12;
+let total = num1 + num2;
+
+console.log(total);
+
+let radius = 10;
+let area = 3.14 * (radius ** 2);
+ 
+console.log(area);   console.log('area = ${area}');
+
+// Assignment Operators:
+let count = 5;
+count += 2;
+
+console.log(count);
+
+let price = 10;
+price *= 2.5;
+
+console.log(price);
+
+// Comparison Operators:
+let age1 = 55;
+let age2 = 68;
+
+console.log(age1 > age2);
+
+let name = "Nika";
+let name2 = "John Doe";
+
+console.log(name != name2);
+
+// Logical Operators:
+  let isMember = true;
+  let hasCoupon = false;
+  let result = (isMember === true || hasCoupon === false || !(isMember === true) || !(hasCoupon === false));
+
+  console.log(result);
+
+  let num = 12;
+  let num3 = 0;
+
+  let result2 = (!( num != 0 || num > 0)); 
+  console.log(result2);
+
+  // Conditional operator.
+
+  let age = 15;
+  let voteAge = 18;
+  let result4 = 15 >= 18 ? ('You are old enough to vote') : ('You are not old enogh to vote'); 
+   console.log(result4);
+
+  let price1 = 10;
+  let productOnSale = false;
+  let result5 = productOnSale === false ? ('The product is not on sale') : ('The product is on sale');
+
+  console.log(result5);
+
+
+
+
+
+
+
+
+
+
+
+
